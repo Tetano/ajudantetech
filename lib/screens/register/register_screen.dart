77890b5fe4f5c7091043feabff2353d2936857d1
VINICIUS VICCO PETRARCA DE ASSIS MIRANDA <vinicius.miranda@tre-to.jus.br>
2020-09-20 18:07:31 -0300

    Tela de Registro finalizada.

--- lib/screens/register/register_screen.dart
+++ lib/screens/register/register_screen.dart
@@ -8,32 +8,101 @@ class RegisterScreen extends StatelessWidget {
 
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
+  String tittle = "Criar Conta";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      body: Center(
-        child: Card(
-          margin: const EdgeInsets.symmetric(horizontal: 16),
-          child: Form(
-            key: formKey,
-            child: Consumer<UserManager>(
-              builder: (_,userManager,__){
-                return ListView(
-                  padding: const EdgeInsets.all(16),
-                  shrinkWrap: true,
-                  children: [
-                    TextFormField(
-
-                    )
-                  ],
-                );
-              }
-            ),
-
-          ),
-        ),
+    return Container(
+      decoration: BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Colors.deepPurple,Colors.red]
+      )
       ),
+        child: Scaffold(
+          key: scaffoldKey,
+          backgroundColor: Colors.transparent,
+          body: Align(
+            alignment: Alignment.center,
+            child: Card(
+              elevation: 30,
+              shadowColor: Colors.pink,
+              color: Colors.pink[50],
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              child: Form(
+                key: formKey,
+                child: Consumer<UserManager>(
+                  builder: (_,userManager,__){
+                    return ListView(
+                      padding: const EdgeInsets.all(16),
+                      shrinkWrap: true,
+                      children: [
+                          TextFormField(
+                          decoration: const InputDecoration(alignLabelWithHint: true,icon: Icon(Icons.email)),
+                          ),
+                        TextFormField(
+                          onTap: (){
+                          },
+                          decoration: const InputDecoration(alignLabelWithHint: true, icon: Icon(Icons.lock),
+
+                        ),
+                        ),
+                        TextFormField(
+                          onTap: (){
+                          },
+                          decoration: const InputDecoration(alignLabelWithHint: true, icon: Icon(Icons.lock_outline),
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(alignLabelWithHint: true,icon: Icon(Icons.person)),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0, 20, 0,0),
+                          child: SizedBox(
+                            height: 40,
+                            child: RaisedButton(
+                              color: Colors.grey[600],
+                              onPressed: (){},
+                              child: Text(
+                                'Registrar-se',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    );
+                  }
+                ),
+              ),
+            ),
+          ),
+          floatingActionButton:
+            CircleAvatar(
+
+              foregroundColor: Colors.pink[50],
+              backgroundColor: Colors.pink[50],
+              radius: 30,
+              child: IconButton(
+                onPressed: (){
+                  scaffoldKey.currentState.showSnackBar(
+                    SnackBar(
+                      content: Text("Não se preocupe, seus dados estão seguros e Criptografados"),
+                      backgroundColor: Colors.blue[300],
+                    )
+                  );
+                },
+                color: Colors.red,
+                icon: Icon(
+                  Icons.security,
+                  color: Colors.red,
+                )
+              )
+            ),
+        ),
     );
   }
 }
